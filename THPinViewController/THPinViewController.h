80a32c7b5f0765ad96cdad514520fb507ffbbda7
Thomas Heß <mail@thomashess.name>
2014-06-27 11:11:16 +0200

    define NS_DESIGNATED_INITIALIZER if not defined

--- THPinViewController/THPinViewController.h
+++ THPinViewController/THPinViewController.h
@@ -7,6 +7,7 @@
 //
 
 @import UIKit;
+#import "THPinViewControllerMacros.h"
 
 @class THPinViewController;
 
---
+++ THPinViewController/THPinViewControllerMacros.h
@@ -0,0 +1,15 @@
+//
+//  THPinViewControllerMacros.h
+//  THPinViewController
+//
+//  Created by Thomas Heß on 27.6.14.
+//  Copyright (c) 2014 Thomas Heß. All rights reserved.
+//
+
+#ifndef NS_DESIGNATED_INITIALIZER
+//#if __has_attribute(objc_designated_initializer)
+//#define NS_DESIGNATED_INITIALIZER __attribute((objc_designated_initializer))
+//#else
+#define NS_DESIGNATED_INITIALIZER
+//#endif
+#endif
